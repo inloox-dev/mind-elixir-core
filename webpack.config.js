a54ca1494d021c247e5e3b866f13485d2e2949e8
Dr. Tiziano Panico <tiziano.panico@inloox.com>
2023-03-03 09:05:07 +0100

    version update

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = (env, argv) => {
       //   new BundleAnalyzerPlugin()
       // ],
       optimization: {
-        minimize: false,
+        minimize: true,
         minimizer: [
           new TerserPlugin({
             terserOptions: {
